7b014cb284977bbe6cca66dfe1ef28bbe5ecf90d
Enrique Smith <enriqueesmith@gmail.com>
2019-03-27 20:07:13 +0000

    second commit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os, copy
 from flask import Flask, jsonify, request
 from flask_migrate import Migrate
-from models import db, Course, Member
+from models import db, Tag, Page
 
 app = Flask(__name__)
 ##Setting the place for the db to run
@@ -12,10 +12,41 @@ db.init_app(app)
 #migration engine
 migrate = Migrate(app, db)
 
-  
+# Get a list of all the Contacts GET /contact/all
 @app.route('/')
 def hello():
-    return 'Hello World'
-  
+    pages = Page.query.all()
+    responseA =[]
+    for p in pages:
+        responseA.append(p.to_dict())
+    tags = Tag.query.all()
+    responseB =[]
+    for t in tags:
+        responseB.append(t.to_dict())
+        
+    return jsonify(responseA, responseB)
+
+# Create a new Contact POST /contact
+
+
+# Get a specific Contact (with the Group objects it belongs to) GET /contact/{contact_id}
+# Delete a Contact DELETE /contact/{contact_id}
+# Update a Contact UPDATE /contact/{contact_id}
+# Get a list of all the Group names and ids GET /group/all
+# Get a specific Group (with all Contact objects related to it) GET /group/{group_id}
+# Update a Group name UPDATE /group/{group_id}
+# Delete a Group DELETE /group/{group_id}
+
+'''
+# Add a Course     - /courses/add  -  POST    
+@app.route('/courses/add', methods=['POST'])
+def addCourses():
+    info = request.get_json() or {}
+    course = Course(name=info['name'])
+    db.session.add(course)
+    db.session.commit()
+    return jsonify({'response': 'Ok'}) 
+'''
+
   
 app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
